34c9098c8a82ee2b0ec8fbde213b54946b3a45d6
Ben <benjamin.schwab.nonspam@gmail.com>
2019-03-06 20:51:13 -0800

    -index now has an earth and moon, they do not move yet

--- js/site.js
+++ js/site.js
@@ -1,8 +1,7 @@
 
  window.addEventListener("DOMContentLoaded", scrollLoop, false); //causes scrollLoop function to be called once at start
 
- var BigYellowCircle = document.getElementById("BigYellowCircle");
- var GreenPentagon = document.getElementById("GreenPentagon");
+
  var BlueSquare = document.getElementById("BlueSquare");
  
  var yScrollPos;
@@ -10,9 +9,10 @@
  function scrollLoop(e){
      yScrollPos = window.scrollY; //get scrollbar position
  
-     parallaxTranslate(-0.2, BigYellowCircle); //negative because scrollbar moves down and I want the element to move up
-     parallaxTranslate(-0.4, GreenPentagon);
-     parallaxTranslate(-0.6, BlueSquare);
+    //  parallaxTranslate(-0.2, BigYellowCircle); //negative because scrollbar moves down and I want the element to move up
+    //  parallaxTranslate(-0.4, GreenPentagon);
+    //  parallaxTranslate(-0.6, BlueSquare);
+        
  
      requestAnimationFrame(scrollLoop); //This is what makes the function loop
  }
